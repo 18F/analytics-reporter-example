dd34140c06f3f1c5417787c2b277b1d49860662e
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-02-07 21:26:22 -0500

    first example of using analytics-reporter via import

---
+++ server.js
@@ -0,0 +1,16 @@
+var Analytics = require("analytics-reporter"),
+	CronJob = require('cron').CronJob;
+
+var job = new CronJob({
+	cronTime: '0 * * * * *', 
+	onTick: function() {
+		var analytics = new Analytics();
+		analytics.run({'csv': true, 'json': true}, function() {
+			console.log('Reports Finished')
+		});
+	},
+	start: false,
+	timeZone: 'America/Los_Angeles'
+});
+
+job.start();
