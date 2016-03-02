8cd54b4f9e1518b4bc934d246f6d4965b634562b
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-03-02 17:25:44 -0500

    Updating the script to work with aws

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 ### Set up a user defined service with credentials
 After entering this command you'll be prompted for each credentials individually.
 ```bash
-cf cups analytics-reporter-creds -p "ANALYTICS_KEY_0, ANALYTICS_KEY_1, ANALYTICS_KEY_2, ANALYTICS_KEY_3, ANALYTICS_REPORT_EMAIL, ANALYTICS_REPORT_IDS, ANALYTICS_REPORTS_PATH"
+cf cups analytics-reporter-creds -p "ANALYTICS_KEY_0, ANALYTICS_KEY_1, ANALYTICS_KEY_2, ANALYTICS_KEY_3, ANALYTICS_REPORT_EMAIL, ANALYTICS_REPORT_IDS, ANALYTICS_REPORTS_PATH, AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_BUCKET, AWS_BUCKET_PATH, AWS_CACHE_TIME"
 ```
+
 
 The keys must be split up using ANALYTICS_KEY_0, ANALYTICS_KEY_1, ...
 
--- server.js
+++ server.js
@@ -4,29 +4,42 @@ var Analytics = require("analytics-reporter"),
 		creds = require("cfenv").getAppEnv().getService(process.env.ANALYTICS_CREDS).credentials;
 
 var key = creds.ANALYTICS_KEY_0 + creds.ANALYTICS_KEY_1 + creds.ANALYTICS_KEY_2 + creds.ANALYTICS_KEY_3;
+var aws_settings = {
+	bucket: creds.AWS_BUCKET,
+  path: creds.AWS_BUCKET_PATH,
+  cache: 0,
+}
 var analytics = new Analytics(
 	key.replace(/\\n/g, '\n'),
 	creds.ANALYTICS_REPORT_EMAIL,
 	creds.ANALYTICS_REPORT_IDS,
 	creds.ANALYTICS_REPORTS_PATH,
-	null,
+	aws_settings,
 	true
 );
 
-// Test run
-analytics.run({'csv': true, 'json': true}, function() {
-	console.log('Reports Finished')
+
+var realtimeJobs = new CronJob({
+  	cronTime: '*/2 * * * *',
+  	onTick: function() {
+			analytics.run({'csv': true, 'json': true, 'only': 'realtime', 'publish': true}, function() {
+				console.log('Reports Finished')
+			});
+    },
+    	start: false,
+    	timeZone: 'America/Los_Angeles'
 });
 
-var job = new CronJob({
-	cronTime: '0 */5 * * * *',
-	onTick: function() {
-		analytics.run({'csv': true, 'json': true}, function() {
-			console.log('Reports Finished')
-		});
-	},
-	start: false,
-	timeZone: 'America/Los_Angeles'
+var dailyReports = new CronJob({
+  	cronTime: '0 * * * *', //10 5 * * *
+  	onTick: function() {
+			analytics.run({'csv': true, 'json': true, 'only': 'os', 'publish': true}, function() {
+				console.log('Reports Finished')
+			});
+    },
+    	start: false,
+    	timeZone: 'America/Los_Angeles'
 });
 
-job.start();
+realtimeJobs.start();
+dailyReports.start();
