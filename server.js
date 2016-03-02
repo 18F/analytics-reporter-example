736003341aa3ab0b3cd431c416824afd7b4991c2
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-03-02 15:59:22 -0500

    removing the extra backslash

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var Analytics = require("analytics-reporter"),
 		fs = require('fs'),
 		creds = require("cfenv").getAppEnv().getService(process.env.ANALYTICS_CREDS).credentials;
 
+var key = creds.ANALYTICS_KEY_0 + creds.ANALYTICS_KEY_1 + creds.ANALYTICS_KEY_2 + creds.ANALYTICS_KEY_3;
 var analytics = new Analytics(
-	key,
+	key.replace(/\\n/g, '\n'),
 	creds.ANALYTICS_REPORT_EMAIL,
 	creds.ANALYTICS_REPORT_IDS,
 	creds.ANALYTICS_REPORTS_PATH,
