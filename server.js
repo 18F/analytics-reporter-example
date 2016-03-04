72835c03cc1af9af38fb125f7cad3b335d26c551
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-03-04 14:13:02 -0500

    Adding async package to allow multiple reports

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var Analytics = require("analytics-reporter"),
     CronJob = require('cron').CronJob,
     fs = require('fs'),
+    async = require("async"),
     creds = require("cfenv").getAppEnv().getService(process.env.ANALYTICS_CREDS).credentials;
 
 var key = creds.ANALYTICS_KEY_0 + creds.ANALYTICS_KEY_1 + creds.ANALYTICS_KEY_2 + creds.ANALYTICS_KEY_3;
@@ -9,7 +10,8 @@ var aws_settings = {
     path: creds.AWS_BUCKET_PATH,
     cache: 0,
 }
-var analytics = new Analytics(
+
+var analyticsDHS = new Analytics(
     key.replace(/\\n/g, '\n'),
     creds.ANALYTICS_REPORT_EMAIL,
     creds.ANALYTICS_REPORT_IDS,
@@ -18,37 +20,68 @@ var analytics = new Analytics(
     true
 );
 
+var dhsRealTime = function(finished) {
+    analyticsDHS.run({
+        'csv': true,
+        'json': true,
+        'frequency': 'realtime',
+        'slim': true,
+        'publish': true
+    }, function() {
+        console.log('Reports Finished')
+    });
+}
+
+var dhsDaily = function(finished) {
+    analyticsDHS.run({
+        'csv': true,
+        'json': true,
+        'frequency': 'daily',
+        'slim': true,
+        'publish': true
+    }, function() {
+        console.log('Reports Finished');
+        finished();
+    });
+}
+
+
+realtimeReports = [dhsRealTime];
+dailyReports = [dhsDaily];
+
+var runAllRealtime = function() {
+    async.each(realtimeReports,
+        function(item, callback) {
+            item(callback);
+        },
+        function(err) {
+            console.log("All Realtime Run");
+        }
+    );
+};
+
+var runAllDaily = function() {
+    async.each(dailyReports,
+        function(item, callback) {
+            item(callback);
+        },
+        function(err) {
+            console.log("All Daily Done");
+        }
+    );
+};
+
 
 var realtimeJobs = new CronJob({
     cronTime: '*/2 * * * *',
-    onTick: function() {
-        analytics.run({
-            'csv': true,
-            'json': true,
-            'frequency': 'realtime',
-            'slim': true,
-            'publish': true
-        }, function() {
-            console.log('Reports Finished')
-        });
-    },
+    onTick: runAllRealtime,
     start: false,
     timeZone: 'America/Los_Angeles'
 });
 
 var dailyReports = new CronJob({
     cronTime: '10 5 * * *',
-    onTick: function() {
-        analytics.run({
-            'csv': true,
-            'json': true,
-            'frequency': 'daily',
-            'slim': true,
-            'publish': true
-        }, function() {
-            console.log('Reports Finished')
-        });
-    },
+    onTick: runAllDaily,
     start: false,
     timeZone: 'America/Los_Angeles'
 });
