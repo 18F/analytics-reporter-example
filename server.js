ef3d469c8f0986f932f9e64c72da8a67334d6aab
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-03-04 13:53:45 -0500

    update server to get all reports

--- server.js
+++ server.js
@@ -1,44 +1,56 @@
 var Analytics = require("analytics-reporter"),
-		CronJob = require('cron').CronJob,
-		fs = require('fs'),
-		creds = require("cfenv").getAppEnv().getService(process.env.ANALYTICS_CREDS).credentials;
+    CronJob = require('cron').CronJob,
+    fs = require('fs'),
+    creds = require("cfenv").getAppEnv().getService(process.env.ANALYTICS_CREDS).credentials;
 
 var key = creds.ANALYTICS_KEY_0 + creds.ANALYTICS_KEY_1 + creds.ANALYTICS_KEY_2 + creds.ANALYTICS_KEY_3;
 var aws_settings = {
-	bucket: creds.AWS_BUCKET,
-  path: creds.AWS_BUCKET_PATH,
-  cache: 0,
+    bucket: creds.AWS_BUCKET,
+    path: creds.AWS_BUCKET_PATH,
+    cache: 0,
 }
 var analytics = new Analytics(
-	key.replace(/\\n/g, '\n'),
-	creds.ANALYTICS_REPORT_EMAIL,
-	creds.ANALYTICS_REPORT_IDS,
-	creds.ANALYTICS_REPORTS_PATH,
-	aws_settings,
-	true
+    key.replace(/\\n/g, '\n'),
+    creds.ANALYTICS_REPORT_EMAIL,
+    creds.ANALYTICS_REPORT_IDS,
+    creds.ANALYTICS_REPORTS_PATH,
+    aws_settings,
+    true
 );
 
 
 var realtimeJobs = new CronJob({
-  	cronTime: '*/2 * * * *',
-  	onTick: function() {
-			analytics.run({'csv': true, 'json': true, 'only': 'realtime', 'publish': true}, function() {
-				console.log('Reports Finished')
-			});
+    cronTime: '*/2 * * * *',
+    onTick: function() {
+        analytics.run({
+            'csv': true,
+            'json': true,
+            'frequency': 'realtime',
+            'slim': true,
+            'publish': true
+        }, function() {
+            console.log('Reports Finished')
+        });
     },
-    	start: false,
-    	timeZone: 'America/Los_Angeles'
+    start: false,
+    timeZone: 'America/Los_Angeles'
 });
 
 var dailyReports = new CronJob({
-  	cronTime: '10 5 * * *',
-  	onTick: function() {
-			analytics.run({'csv': true, 'json': true, 'only': 'os', 'publish': true}, function() {
-				console.log('Reports Finished')
-			});
+    cronTime: '10 5 * * *',
+    onTick: function() {
+        analytics.run({
+            'csv': true,
+            'json': true,
+            'frequency': 'daily',
+            'slim': true,
+            'publish': true
+        }, function() {
+            console.log('Reports Finished')
+        });
     },
-    	start: false,
-    	timeZone: 'America/Los_Angeles'
+    start: false,
+    timeZone: 'America/Los_Angeles'
 });
 
 realtimeJobs.start();
