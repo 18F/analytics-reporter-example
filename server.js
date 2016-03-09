7aeafb539638f1a0f7cf6fe136543f8ecd316f51
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2016-03-09 11:49:20 -0500

    Updates

--- server.js
+++ server.js
@@ -2,76 +2,85 @@ var Analytics = require("analytics-reporter"),
     CronJob = require('cron').CronJob,
     fs = require('fs'),
     async = require("async"),
-    creds = require("cfenv").getAppEnv().getService(process.env.ANALYTICS_CREDS).credentials;
+    spawn = require('child_process').spawn,
+    creds = require("cfenv").getAppEnv().getService(process.env.ANALYTICS_CREDS).credentials,
+    key = creds.ANALYTICS_KEY_0 + creds.ANALYTICS_KEY_1 + creds.ANALYTICS_KEY_2 + creds.ANALYTICS_KEY_3,
+    realtimeReports = [],
+    dailyReports = [];
 
-var key = creds.ANALYTICS_KEY_0 + creds.ANALYTICS_KEY_1 + creds.ANALYTICS_KEY_2 + creds.ANALYTICS_KEY_3;
-var aws_settings = {
-    bucket: creds.AWS_BUCKET,
-    path: creds.AWS_BUCKET_PATH,
-    cache: 0,
-}
+var reportIDs = [{
+    gaid: 'ga:68909496',
+    path: 'data/sba'
+}, {
+    gaid: 'ga:67460690',
+    path: 'data/dhs'
+}]
 
-var analyticsDHS = new Analytics(
-    key.replace(/\\n/g, '\n'),
-    creds.ANALYTICS_REPORT_EMAIL,
-    creds.ANALYTICS_REPORT_IDS,
-    creds.ANALYTICS_REPORTS_PATH,
-    aws_settings,
-    true
-);
-
-var dhsRealTime = function(finished) {
-    analyticsDHS.run({
-        'csv': true,
-        'json': true,
-        'frequency': 'realtime',
-        'slim': true,
-        'publish': true
-    }, function() {
-        console.log('Reports Finished');
-        finished();
-    });
-}
-
-var dhsDaily = function(finished) {
-    analyticsDHS.run({
-        'csv': true,
-        'json': true,
-        'frequency': 'daily',
-        'slim': true,
-        'publish': true
-    }, function() {
-        console.log('Reports Finished');
-        finished();
-    });
-}
-
-
-realtimeReports = [dhsRealTime];
-dailyReports = [dhsDaily];
+for (idx in reportIDs) {
+    (function(reportID) {
+        var aws_settings = {
+            bucket: creds.AWS_BUCKET,
+            path: reportID.path,
+            cache: 0,
+        };
+        var analytics = new Analytics(
+            key.replace(/\\n/g, '\n'),
+            creds.ANALYTICS_REPORT_EMAIL,
+            reportID.gaid,
+            creds.ANALYTICS_REPORTS_PATH,
+            aws_settings,
+            true
+        );
+        realtimeReports.push(function(finished) {
+            analytics.run({
+                'csv': true,
+                'json': true,
+                'frequency': 'realtime',
+                'slim': true,
+                'publish': true
+            }, function() {
+                console.log('Reports Finished');
+                finished();
+            });
+        });
+        dailyReports.push(function(finished) {
+            analytics.run({
+                'csv': true,
+                'json': true,
+                'frequency': 'daily',
+                'slim': true,
+                'publish': true
+            }, function() {
+                console.log('Reports Finished');
+                finished();
+            });
+        });
+    })(reportIDs[idx]);
+};
 
 var runAllRealtime = function() {
-    async.each(realtimeReports,
+    async.eachSeries(realtimeReports,
         function(item, callback) {
+            console.log("Realtime Reports Starting");
             item(callback);
         },
         function(err) {
-            console.log("All Realtime Run");
+            console.log("Error");
         }
     );
 };
 
 var runAllDaily = function() {
-    async.each(dailyReports,
+	async.eachSeries(dailyReports,
         function(item, callback) {
+            console.log("Daily Reports Starting");
             item(callback);
         },
         function(err) {
-            console.log("All Daily Done");
+            console.log("Error");
         }
     );
 };
-
 
 var realtimeJobs = new CronJob({
     cronTime: '*/2 * * * *',
@@ -80,7 +89,7 @@ var realtimeJobs = new CronJob({
     timeZone: 'America/Los_Angeles'
 });
 
-var dailyReports = new CronJob({
+var dailyReportJobs = new CronJob({
     cronTime: '10 5 * * *',
     onTick: runAllDaily,
     start: false,
@@ -88,4 +97,4 @@ var dailyReports = new CronJob({
 });
 
 realtimeJobs.start();
-dailyReports.start();
+dailyReportJobs.start();
